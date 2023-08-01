eb0b18e6dc662acf84df937b8315102a1ce8214d
Vang Nguyen <ngthikimvangg@gmail.com>
2023-08-01 11:36:38 +0700

    fix next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
-      },
-    ]
-  }
+  reactStrictMode: true,
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://localhost:5000/api/:path*',
+  //     },
+  //   ]
+  // }
 }
 
 module.exports = nextConfig
