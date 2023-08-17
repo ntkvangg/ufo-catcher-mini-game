1d9f3e0387e26baa6755dae3fde57e3ef37f61f5
Vang Nguyen <ngthikimvangg@gmail.com>
2023-08-17 18:14:15 +0700

    fix socket io

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const io = new Server(server, {
         methods: ["GET", "POST"], 
         allowedHeaders: ["Access-Control-Allow-Origin"]
     },
-    transports: ["websocket"]
+    transports: ["polling", "websocket"]
 });
 
 io.on("connection", (socket)=>{
