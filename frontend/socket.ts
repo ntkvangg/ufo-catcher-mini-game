861e7cf7b0aa78546abbd9fb05ddcf54797990bf
Vang Nguyen <ngthikimvangg@gmail.com>
2023-08-17 17:55:17 +0700

    change server backend

--- frontend/socket.ts
+++ frontend/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? "https://ufo-catcher-mini-game-backend.vercel.app" : 'http://localhost:5000';
+const URL = process.env.NODE_ENV === 'production' ? "https://ufo-catcher-mini-game-backend.vercel.app/" : 'http://localhost:5000';
 
 export const socket = io(URL);
