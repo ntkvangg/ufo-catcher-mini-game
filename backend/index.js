1e6592a985774a513ae49a24fc007ae34abd0704
Vang Nguyen <ngthikimvangg@gmail.com>
2023-08-18 11:21:28 +0700

    config socket

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const io = new Server(server, {
     path: '/api/socket',
     addTrailingSlash: false
 });
-
+io.set("origins", '*:*');
 io.on("connection", (socket)=>{
     socket.on("catch-animal-success", (data)=>{
         socket.broadcast.emit("receive-catch-animal", data);
